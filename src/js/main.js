19132311f35a80815e577d51033cb3dfe81a934d
David Sudia <dsudia@gmail.com>
2016-01-27 16:20:48 -0700

    fix bug with weather info not displaying

--- src/js/main.js
+++ src/js/main.js
@@ -74,8 +74,21 @@ jQuery(document).ready(function () {
           currentHumid.push(Math.round(data['main']['humidity']));
           todayLow.push(Math.round(data['main']['temp_min']));
           todayHigh.push(Math.round(data['main']['temp_max']));
-          todayCondition.push(Math.round(data['weather']['description']));
-          todayIcon.push(Math.round(data['weather']['icon']));
+          todayCondition.push(data['weather'][0]['description']);
+          todayIcon.push(data['weather'][0]['icon']);
+             // Move this back once I learn promises.
+          var weatherP1 = $('#temp');
+          var weatherP2 = $('#low');
+          var weatherP3 = $('#high');
+          var weatherP4 = $('#humid');
+          var weatherP5 = $('#condition');
+          var weatherIcon = $('#weather-icon');
+          $(weatherP1).html('The current temperature is ' + currentTemp[0] + ' degrees Fahrenheit');
+          $(weatherP2).html('Today\'s low temperature is ' + todayLow[0] + ' degrees Fahrenheit');
+          $(weatherP3).html('Today\'s high temperature is ' + todayHigh[0] + ' degrees Fahrenheit');
+          $(weatherP4).html('The current humidity level is ' + todayHigh[0] + '%');
+          $(weatherP5).html('The current weather condition is ' + todayCondition[0]);
+          $(weatherIcon).html('<img alt = ' + todayCondition[0] + 'src="http://openweathermap.org/img/w/"' + todayIcon[0]);
         }
       });
     }
@@ -94,18 +107,18 @@ var todayCondition = [];
 var todayIcon = [];
 
 //Put variable values into DOM
-jQuery(document).ready(function () {
-  var weatherP1 = $('#weather p:first-child');
-  var weatherP2 = $(weatherP1).next;
-  var weatherP3 = $(weatherP2).next;
-  var weatherP4 = $(weatherP3).next;
-  var weatherP5 = $(weatherP4).next
-  $(weatherP1).html('The current temperature is ' + currentTemp[0] + 'degrees Fahrenheit');
-  $(weatherP2).html('Today\'s low temperature is ' + todayLow[0] + ' degrees Fahrenheit');
-  $(weatherP3).html('Today\'s high temperature is ' + todayHigh[0] + ' degrees Fahrenheit');
-  $(weatherP4).html('The current humidity level is ' + todayHigh[0] + '%');
-  $(weatherP5).html('The current weather condition is ' + todayCondition[0] + ' ' + <img alt=todayCondition[0] src='http://openweathermap.org/img/w/' + todayIcon[0]);
-});
+// jQuery(document).ready(function () {
+//   var weatherP1 = $('#weather p:first-child');
+//   var weatherP2 = $(weatherP1).next;
+//   var weatherP3 = $(weatherP2).next;
+//   var weatherP4 = $(weatherP3).next;
+//   var weatherP5 = $(weatherP4).next
+//   $(weatherP1).html('The current temperature is ' + currentTemp[0] + 'degrees Fahrenheit');
+//   $(weatherP2).html('Today\'s low temperature is ' + todayLow[0] + ' degrees Fahrenheit');
+//   $(weatherP3).html('Today\'s high temperature is ' + todayHigh[0] + ' degrees Fahrenheit');
+//   $(weatherP4).html('The current humidity level is ' + todayHigh[0] + '%');
+//   $(weatherP5).html('The current weather condition is ' + todayCondition[0] + ' ' + <img alt=todayCondition[0] src='http://openweathermap.org/img/w/' + todayIcon[0]);
+// });
 
 //AJAX call for today's weather conditions
 //  FIX THIS ONCE I LEARN PROMISES
