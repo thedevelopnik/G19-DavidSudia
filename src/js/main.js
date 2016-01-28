e5369356915d05ac029086e3f3bf54a59de330ad
David Sudia <dsudia@gmail.com>
2016-01-28 14:52:00 -0700

    add calendar functionality

--- src/js/main.js
+++ src/js/main.js
@@ -52,7 +52,7 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 
 // ** Begin Location by IP ** //
 // Establish variable to hold latitude and longitude for use in Open Weather
-var userLocation = []
+var userLocation = [];
 
 // Make ajax request to get latitude and longitude
 jQuery(document).ready(function () {
@@ -140,46 +140,106 @@ var todayIcon = [];
 
 
 // ** Begin Google Calendar ** //
+var CLIENT_ID = '723061971439-dv1pbtq741e2dk5n05psbrnufa99p4cj';
 
-// get token
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail());
+var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+
+/**
+ * Check if current user has authorized this application.
+ */
+function checkAuth() {
+  gapi.auth.authorize(
+    {
+      'client_id': CLIENT_ID,
+      'scope': SCOPES.join(' '),
+      'immediate': true
+    }, handleAuthResult);
 }
 
-// create the button and scopes
-function onSuccess(googleUser) {
-  console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+/**
+ * Handle response from authorization server.
+ *
+ * @param {Object} authResult Authorization result.
+ */
+function handleAuthResult(authResult) {
+  var authorizeDiv = document.getElementById('authorize-div');
+  if (authResult && !authResult.error) {
+    // Hide auth UI, then load client library.
+    // authorizeDiv.style.display = 'none';
+    loadCalendarApi();
+  } else {
+    // Show auth UI, allowing the user to initiate authorization by
+    // clicking authorize button.
+    authorizeDiv.style.display = 'inline';
+  }
 }
 
-function onFailure(error) {
-  console.log(error);
+/**
+ * Initiate auth flow in response to user clicking authorize button.
+ *
+ * @param {Event} event Button click event.
+ */
+function handleAuthClick(event) {
+  gapi.auth.authorize(
+    {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
+    handleAuthResult);
+  return false;
 }
 
-function renderButton() {
-  gapi.signin2.render('my-signin2', {
-    'scope': 'https://www.googleapis.com/auth/plus.login',
-    'scope': 'https://www.googleapis.com/auth/calendar.readonly',
-    'width': 200,
-    'height': 50,
-    'longtitle': true,
-    'theme': 'dark',
-    'onsuccess': onSuccess,
-    'onfailure': onFailure
+/**
+ * Load Google Calendar client library. List upcoming events
+ * once client library is loaded.
+ */
+function loadCalendarApi() {
+  gapi.client.load('calendar', 'v3', listUpcomingEvents);
+}
+
+/**
+ * Print the summary and start datetime/date of the next ten events in
+ * the authorized user's calendar. If no events are found an
+ * appropriate message is printed.
+ */
+function listUpcomingEvents() {
+  var request = gapi.client.calendar.events.list({
+    'calendarId': 'primary',
+    'timeMin': (new Date()).toISOString(),
+    'showDeleted': false,
+    'singleEvents': true,
+    'maxResults': 10,
+    'orderBy': 'startTime'
+  });
+
+  request.execute(function(resp) {
+    var events = resp.items;
+    appendPre('Upcoming events:');
+
+    if (events.length > 0) {
+      for (i = 0; i < events.length; i++) {
+        var event = events[i];
+        var when = event.start.dateTime;
+        if (!when) {
+          when = event.start.date;
+        }
+        appendPre(event.summary + ' (' + when + ')');
+      }
+    } else {
+      appendPre('No upcoming events found.');
+    }
+
   });
 }
 
-// allow sign-out
-function signOut() {
-  var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function () {
-    console.log('User signed out.');
-  });
+/**
+ * Append a pre element to the body containing the given message
+ * as its text node.
+ *
+ * @param {string} message Text to be placed in pre element.
+ */
+function appendPre(message) {
+  var pre = document.getElementById('output');
+  var textContent = document.createTextNode(message + '\n');
+  pre.appendChild(textContent);
 }
-
 // ** End Google Calendar ** //
 
 
