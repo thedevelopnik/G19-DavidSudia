ae353fde6a0699682e9d7a26432945f7e9ce7fc5
David Sudia <dsudia@gmail.com>
2016-01-28 11:23:57 -0700

    Work on google sign in button

--- src/js/main.js
+++ src/js/main.js
@@ -47,7 +47,6 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
                         'Error: The Geolocation service failed.' :
                         'Error: Your browser doesn\'t support geolocation.');
 }
-
 // ** End Google Map ** //
 
 
@@ -141,6 +140,45 @@ var todayIcon = [];
 
 
 // ** Begin Google Calendar ** //
+
+// get token
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+}
+
+// create the button and scopes
+function onSuccess(googleUser) {
+  console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+}
+
+function onFailure(error) {
+  console.log(error);
+}
+
+function renderButton() {
+  gapi.signin2.render('my-signin2', {
+    'scope': 'https://www.googleapis.com/auth/plus.login',
+    'scope': 'https://www.googleapis.com/auth/calendar.readonly',
+    'width': 200,
+    'height': 50,
+    'longtitle': true,
+    'theme': 'dark',
+    'onsuccess': onSuccess,
+    'onfailure': onFailure
+  });
+}
+
+// allow sign-out
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+}
 
 // ** End Google Calendar ** //
 
