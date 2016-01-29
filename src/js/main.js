92844fcf8a235b8ca163e7dcc9f9e723bb2fb61b
David Sudia <dsudia@gmail.com>
2016-01-29 14:53:37 -0700

    make basic changes to incorporate masonry library

--- src/js/main.js
+++ src/js/main.js
@@ -4,69 +4,22 @@ var body = document.getElementsByTagName('body')[0];
 
 var header = document.getElementsByTagName('header')[0];
 
-var halfBox = '<div class="col-md-1"></div><section class="col-md-4 half-section"></section><div class="col-md-1"></div>';
+var grid = document.getElementById('main-grid');
 
-var fullBox = '<div class="col-md-1"></div><section class="col-md-10 full-section"></section><div class="col-md-1"></div>';
+var gridItemSml = '<div class="grid-item"></div>';
 
-var createRow = '<div class="container-fluid"><div class="row"></div></div>';
+var gridItemMed = '<div class="grid-item--width-2"></div>';
 
-var row = document.getElementsByClassName('row');
+var gridItemLg = '<div class="grid-item--widht-3"></div>';
 
-var halfSections = document.getElementsbyClassName('half-section');
-
-var fullSections = document.getElementsbyClassName('full-section');
-
-// Functions for inserting and removing a half-screen widget
-
-function insertHalfWidget() {
-  if (halfSections[0] === undefined) {
-    createHalfWidgetSpace();
-    searchForEmptyHalfSection();
-  } else if ('there are some but none are empty') {
-
-  } else {
-    searchForEmptyHalfSection();
-  }
-}
-
-function searchForEmptyHalfSection(createWidget) {
-  for (var i = 0; i < halfSections.length; i++) {
-    if (halfSections[i].innerHTML === '') {
-      halfSections[i].innerHTML(createWidget());
-    }
-  }
-}
-
-function createHalfWidgetSpace() {
-  newRow = body.appendChild(createRow);
-  newRow.appendChild(halfBox);
-  newRow.appendChild(halfBox);
-}
-
-function removeHalfWidget() {
-
-}
-
-// Functions for creating and removing a full screen widget
-
-function insertFullWidget() {
-  createFullWidgetSpace();
-}
-
-function createFullWidgetSpace() {
-  newRow = body.appendChild(createRow);
-  newRow.appendChild(fullBox);
-}
-
-
-
-function removeFullWidget() {
+function removeWidget() {
 
 }
 
 // Functions for creating each widget
 
 function createMapWidget() {
+  grid.appendChild(gridItemMed);
 
 }
 
